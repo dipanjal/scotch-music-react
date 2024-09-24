047882acdbb1dba1851a72037bce702859b77bae
dipanjal <dipanjalmaitra@gmail.com>
2024-09-24 17:21:37 +0600

    Scotch UI Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div className="scotch-container">
+              <nav>
+                  <a href="#">CONTACT</a>
+                  <a href="#">ABOUT</a>
+                  <a href="#" className="buy">BUY NOW</a>
+              </nav>
+              <div className="main-container">
+                  <div className="text-sphere-container">
+                      <div className="text-container">
+                          <h1>Scotch</h1>
+                          <h2>VIDEOCASSETTE EG</h2>
+                          <h3>T 120</h3>
+                          <p>Designed by the inventors of videotape, this Scotch EG Videocasette gives your superior
+                              performance</p>
+                          <a href="https://www.amazon.com/-/es/Scotch-Cinta-blanco-T-120-est%C3%A1ndar/dp/B008C7Z54Y"
+                             className="buy-cta">Buy Now</a>
+                      </div>
+                      <a href="https://www.amazon.com/-/es/Scotch-Cinta-blanco-T-120-est%C3%A1ndar/dp/B008C7Z54Y">
+                          <div className="sphere"></div>
+                      </a>
+                  </div>
+                  <div className="learn-arrow">
+                      <div className="learn">LEARN MORE</div>
+                      <div className="arrow">🢓</div>
+                  </div>
+              </div>
+          </div>
+      </>
   )
 }
 
