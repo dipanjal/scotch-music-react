fbe74aea78408d5be0965ea7c6c4689cd4f5d981
dipanjal <dipanjalmaitra@gmail.com>
2024-09-25 23:36:39 +0600

    sourced statis audio file

--- src/components/core/AudioPlayer.jsx
+++ src/components/core/AudioPlayer.jsx
@@ -29,7 +29,7 @@ const AudioPlayer = forwardRef(({ loop = true }, ref) => {
                onEnded={(e) => audioOnEnded(e, loop)}
         >
             <source
-                src="https://pouch.jumpshare.com/preview/KYulVPaNAQKyAjcvCohtZDrgmBpsqJfaKsAC0KB8v2pc9EzYJYFb7G0cGcWXkcgpWJIOnWbQZgdC2EZ6AzKNHEBi_KNI9zBMUeo99KDVOzu15sUIgL_jNrVeGnE94bfdXTUd6xCp6gBi9kbcxN31sW6yjbN-I2pg_cnoHs_AmgI.mp3"
+                src="/music.mp3"
                 type="audio/mp3"
             />
             Your browser does not support the audio element.
