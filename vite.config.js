6884ff5b7987288e7d1e142777e7ab9c52d8ea97
dipanjal <dipanjalmaitra@gmail.com>
2024-09-30 01:52:43 +0600

    GHPG-1: gh action deployment config added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import AppConfig from "./app.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/scotch-music-react/",
   server: AppConfig.dev
 })
